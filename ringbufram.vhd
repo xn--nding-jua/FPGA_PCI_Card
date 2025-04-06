b91b93be6698416fc25707f3421ce8e871461a20
Dr. Christian Nöding <christian@noeding-online.de>
2025-04-06 17:10:40 +0200

    Working FIFO Buffer for external SRAM

--- ringbufram.vhd
+++ ringbufram.vhd
@@ -1,11 +1,14 @@
--- FIFO for Audio-data
--- Author: Jonas Julian Jensen, 17.06.2019
--- Source: https://vhdlwhiz.com/ring-buffer-fifo/
+-- FIFO RingBuffer RAM-Controller
+-- v1.0.0 built on 06.04.2025
+-- (c) 2025 Chris Noeding
+-- https://github.com/xn-nding-jua/FPGA_PCI_Card
+--
+-- Parts of this code by Jonas Julian Jensen, 17.06.2019
+-- https://vhdlwhiz.com/ring-buffer-fifo/
 
 library ieee;
 use ieee.std_logic_1164.all;
 use IEEE.NUMERIC_STD.ALL;
-use ieee.math_real.all;
 
 entity ringbufram is
 	generic (
@@ -35,21 +38,21 @@ entity ringbufram is
 		fill_count	: out unsigned(RAM_SIZE - 1 downto 0);
 		
 		-- signals to external SRAM
-		ram_ad		: out std_logic_vector(RAM_SIZE - 1 downto 0);
-		ram_data	: inout std_logic_vector(RAM_WIDTH - 1 downto 0);
-		ram_par		: inout std_logic_vector(1 downto 0);
-		ram_nADSP	: out std_logic;
-		ram_nADSC	: out std_logic;
-		ram_nWH		: out std_logic;
-		ram_nWL		: out std_logic;
-		ram_nADV	: out std_logic;
-		ram_nOE		: out std_logic;
-		ram_nCS		: out std_logic
+		ram_ad		: out std_logic_vector(RAM_SIZE - 1 downto 0); -- RAM-address
+		ram_data	: inout std_logic_vector(RAM_WIDTH - 1 downto 0); -- RAM-data
+		ram_par		: inout std_logic_vector(1 downto 0); -- parity. Not used here
+		ram_nADSP	: out std_logic; --
+		ram_nADSC	: out std_logic; --
+		ram_nWH		: out std_logic; -- WriteEnable High-Byte
+		ram_nWL		: out std_logic; -- WriteEnable Low-Byte
+		ram_nADV	: out std_logic; -- automatic address incrementation. Not used here
+		ram_nOE		: out std_logic; -- output enable
+		ram_nCS		: out std_logic -- ChipSelect
 	);
 end ringbufram;
 
 architecture rtl of ringbufram is
-	type t_SM_Ringbuffer is (s_Idle, s_Write, s_PrepareRead, s_Read);
+	type t_SM_Ringbuffer is (s_Idle, s_ReadPrepare, s_Read);
 	signal s_SM_Ringbuffer : t_SM_Ringbuffer := s_Idle;
 
 	subtype index_type is integer range 0 to (2**RAM_SIZE - 1);
@@ -60,8 +63,7 @@ architecture rtl of ringbufram is
 	signal full_i	: std_logic;
 	signal fill_count_i	: integer range 0 to (2**RAM_SIZE - 1);
 
-	signal queueRead	: std_logic;
-	signal readData		: std_logic;
+	signal rd_queued			: std_logic;
 	
 	-- Increment and wrap
 	procedure incr(signal index : inout index_type) is
@@ -78,24 +80,42 @@ begin
 	full <= full_i;
 	fill_count <= to_unsigned(fill_count_i, fill_count'length);
 	
-	-- Set the flags
+	-- set the flags
 	empty_i <= '1' when fill_count_i = 0 else '0';
 	empty_next <= '1' when fill_count_i <= 1 else '0';
 	full_i <= '1' when fill_count_i >= (2**RAM_SIZE - 1) else '0';
 	full_next <= '1' when fill_count_i >= (2**RAM_SIZE - 2) else '0';
-	
+
+	-- process the RAM-functions
 	proc_ram : process(clk)
 	begin
 		if rising_edge(clk) then
 			if rst = '1' then
-				head <= 0;
-
-				tail <= 0;
+				-- reset outputs
 				rd_valid <= '0';
+			else
+				if (s_SM_Ringbuffer = s_Idle) then
+					-- nothing to do here
+					rd_valid <= '0';
+				elsif (s_SM_Ringbuffer = s_ReadPrepare) then
+					-- wait that RAM reads the desired address
+					rd_valid <= '0';
+				elsif (s_SM_Ringbuffer = s_Read) then
+					-- read data from RAM
+					rd_data <= ram_data;
+					rd_valid <= '1';
+				end if;
+			end if;
+		end if;
+		
+		if falling_edge(clk) then
+			if rst = '1' then
+				-- reset internal signals
+				head <= 0;
+				tail <= 0;
 
-				readData <= '0';
-
-				-- reset RAM-outputs
+				-- reset outputs to RAM
+				ram_data <= (others => 'Z');
 				ram_nADSP <= '1';
 				ram_nADSC <= '1';
 				ram_nWH <= '1';
@@ -104,13 +124,13 @@ begin
 				ram_nOE <= '1';
 				ram_nCS <= '1';
 
+				-- reset state-machine
 				s_SM_Ringbuffer <= s_Idle;
 			else
 				if (s_SM_Ringbuffer = s_Idle) then
-					rd_valid <= '0';
-					
+					-- check if we have to write to RAM
 					if wr_en = '1' and full_i = '0' then
-						-- write data to RAM. Write takes precidence over read
+						-- write data to RAM
 						ram_ad <= std_logic_vector(to_unsigned(head, ram_ad'length));
 						ram_data <= wr_data;
 						ram_nCS <= '0';
@@ -118,52 +138,53 @@ begin
 						ram_nWH <= '0';
 						ram_nWL <= '0';
 						ram_nOE <= '1';
-						
 						incr(head);
 
-						-- if a read-request occurs right here, queue it
-						queueRead <= rd_en;
-					else
-						-- turn-off outputs
-						ram_data <= (others => 'Z');
-						
-						-- check if we need to read from RAM
-						if (rd_en = '1' or queueRead = '1') and empty_i = '0' then
-							-- we have a pending read-operation
-							queueRead <= '0';
-
-							ram_ad <= std_logic_vector(to_unsigned(tail, ram_ad'length));
-							ram_nCS <= '0';
-							ram_nADSC <= '0';
-							s_SM_Ringbuffer <= s_Read;
-						else
-							-- no read or write operation
-							ram_nCS <= '1';
-							ram_nADSC <= '1';
+						-- as write takes precidence over read, we queue any read-request
+						if (rd_en = '1') then
+							rd_queued <= '1';
 						end if;
-
-						readData <= '0';
+					elsif (rd_en = '1' or rd_queued = '1') and empty_i = '0' then
+						-- we have a current or queued read-request
+						rd_queued <= '0';
+						
+						-- set desired address and enable RAM
+						ram_ad <= std_logic_vector(to_unsigned(tail, ram_ad'length));
+						ram_nCS <= '0';
+						ram_nADSC <= '0';
 						ram_nWH <= '1';
 						ram_nWL <= '1';
 						ram_nOE <= '0';
+						incr(tail);
+						
+						s_SM_Ringbuffer <= s_ReadPrepare;
+					else
+						-- no read or write, so nothing to do -> turnoff outputs and set all values to default
+						ram_data <= (others => 'Z');
+						ram_nCS <= '1';
+						ram_nADSC <= '1';
+						ram_nWH <= '1';
+						ram_nWL <= '1';
+						ram_nOE <= '1';
 					end if;
+				elsif (s_SM_Ringbuffer = s_ReadPrepare) then
+					-- RAM has read the desired address, so go to read-state
+					s_SM_Ringbuffer <= s_Read;
 				elsif (s_SM_Ringbuffer = s_Read) then
-					rd_data <= ram_data;
-				
-					incr(tail);
-					rd_valid <= '1';
+					-- read is done on rising edge
+					s_SM_Ringbuffer <= s_Idle;
 				end if;
 			end if;
 		end if;
 	end process;
 
---	-- Update the fill count
---	proc_count : process(head, tail)
---	begin
---		if head < tail then
---			fill_count_i <= head - tail + RAM_DEPTH;
---		else
---			fill_count_i <= head - tail;
---		end if;
---	end process;
+	-- update the fill count
+	proc_count : process(head, tail)
+	begin
+		if head < tail then
+			fill_count_i <= head - tail + 2**RAM_SIZE;
+		else
+			fill_count_i <= head - tail;
+		end if;
+	end process;
 end rtl;
